981848c9eabec4b3f055fdfd3bf4e04398037c43
Ivan Morales <IvanJohnMorales@Gmail.com>
2022-05-14 10:55:06 -0400

    Updated home page

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
-import './styles/HomePageView.css'
+import './styles/HomePageView.css';
+
+
 
 
 
@@ -9,10 +11,12 @@ function HomePageView() {
     return (
         <div>
             <nav className='navbar'>
+                <Link to='/' className='linkHome'>CMS CAMPUS MANAGEMENT SYSTEM</Link>
                 <Link to='/' className='link'>HOME</Link>
                 <Link to='/campuses' className='link'>CAMPUSES</Link>
                 <Link to='/students' className='link'>STUDENTS</Link>
             </nav>
+            <p>Select an option in the top right corner to get started</p>
         </div>
     );
 }
